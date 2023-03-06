c17485a08d626771d0d069e81fee8efc6a254b94
khg0710 <hom0710@naver.com>
2023-03-06 15:27:56 +0900

    테스트케이스, 출력을 모니터에 하지않고 문자열얻기

--- src/test/java/com/ll/Apptests.java
+++ src/test/java/com/ll/Apptests.java
@@ -3,6 +3,7 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +17,19 @@ public class Apptests {
         String cmd =sc.nextLine().trim();
         assertThat(cmd).isEqualTo("안녕");
 
+    }
+    @Test
+    @DisplayName("출력을_모니터에_하지_않고_문자열로_얻기")
+    public void t2() {
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        System.out.print("안녕");
+
+        String rs = output.toString();
+
+        TestUtil.clearSetOutToByteArray(output);
+
+        assertThat(rs).isEqualTo("안녕");
     }
 
 }
